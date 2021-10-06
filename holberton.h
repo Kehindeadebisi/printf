caa177642e15684bbbfe9238e90f482954e3c465
hollaberry <you@example.com>
2021-10-06 09:26:52 -0700

    creating functions file

---
+++ holberton.h
@@ -0,0 +1,24 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+
+int _putchar(char c);
+
+
+
+
+
+
+/**
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
+{
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
+
+#endif
