d5189d4824ffbc1f998ad3601ba542f38368ec57
hollaberry <you@example.com>
2021-10-07 05:04:20 -0700

    update main header

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ int _printf(const char *format, ...);
 int print_char(va_list arg);
 int print_int(va_list arg);
 int print_str(va_list arg);
-
+int print_unsignedIntToHex(unsigned int num, char _case);
 /**
  * struct identifierStruct - structure definition of a printTypeStruct
  * @indentifier: type
