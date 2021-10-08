09c8e23b5cadfcf4d965291bc422fc29f69869f3
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-10-08 07:13:28 -0700

    fixed some lines

--- link_func.c
+++ link_func.c
@@ -5,24 +5,25 @@
  * @format: a character to check
  * Return: a pointer to the function
  */
+
 int (*get_func(const char *format))(va_list)
 {
-int i;
-func_t p[] = {
-{"c", print_char},
-{"s", print_str},
-{"%", print_percent},
-{"d", print_dec},
-{"i", print_dec},
-{NULL, NULL}
-};
+	int i;
+	func_t p[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"%", print_pct},
+		{"d", print_dec},
+		{"i", print_dec},
+		{NULL, NULL}
+	};
 
-for (i = 0; p[i].t; i++)
-{
-if (*format == *(p[i].t))
-{
-return (p[i].f);
-}
-}
-return (NULL);
+	for (i = 0; p[i].t; i++)
+	{
+		if (*format == *(p[i].t))
+		{
+			return (p[i].f);
+		}
+	}
+	return (NULL);
 }
